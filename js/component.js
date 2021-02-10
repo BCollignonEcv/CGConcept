24d1aa7e2bb2520b731fc9b10c8649b5d043fa80
BCollignonEcv <baptiste.collignon@mail-ecv.fr>
2021-02-10 12:30:39 +0100

    create menu and update page pumptrack terre

--- js/component.js
+++ js/component.js
@@ -1,4 +1,17 @@
 /************ HEADER ************/ 
-$('header .c_nav-icon').click(function(){
-    $('.c_nav-aside').toggleClass('active');
+$('header #menu').click(function(){
+    var menu = $('.c_menu-aside').toggleClass('active');
+    if(menu.hasClass('active')){
+        setTimeout(function() {
+            $('#menu').text('close');
+        }, 500);
+    }else{
+        setTimeout(function() {
+            $('#menu').text('menu');
+        }, 100);
+    }
 });
+
+function echoToto(){
+    console.log('toto');
+}
