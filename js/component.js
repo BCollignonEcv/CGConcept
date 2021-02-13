4fe8612d36082628a67f66a159b9fd7c10d682ff
BCollignonEcv <baptiste.collignon@mail-ecv.fr>
2021-02-14 00:07:21 +0100

    update all footer and header

--- js/component.js
+++ js/component.js
@@ -2,16 +2,54 @@
 $('header #menu').click(function(){
     var menu = $('.c_menu-aside').toggleClass('active');
     if(menu.hasClass('active')){
-        setTimeout(function() {
-            $('#menu').text('close');
-        }, 500);
+        animateOpeningMenu();
     }else{
-        setTimeout(function() {
-            $('#menu').text('menu');
-        }, 100);
+        animateClosingMenu();
     }
 });
 
-function echoToto(){
-    console.log('toto');
+function animateOpeningMenu(){
+    var body = $('body');
+    var menu = $('.c_menu-aside');
+
+    // Animate body to the left
+    body.animate({
+        marginLeft: '-50vw'
+    }, 400);
+
+    // Animate menu to the left
+    menu.animate({
+        left: '0'
+    },{
+        duration: 300,
+        specialEasing: {
+          left: "swing"
+        }
+    });
+
+    setTimeout(function() {
+        $('#menu').text('close');
+    }, 500);
+}
+
+function animateClosingMenu(){
+    var body = $('body');
+    var menu = $('.c_menu-aside');
+    // Animate body to normal
+    body.animate({
+        marginLeft: '0'
+    }, 300);
+
+    // Animate menu to the right
+    menu.animate({
+        left: '100vw'
+    },{
+        duration: 400,
+        specialEasing: {
+          left: "swing"
+        }
+    });
+    setTimeout(function() {
+        $('#menu').text('menu');
+    }, 100);
 }
